8b8495c30d0105fd4c6587a1bb26f2f64a76ab2a
lizheng9824 <lizheng@outlook.jp>
2021-06-19 20:54:10 +0900

    initialize springboot

---
+++ src/main/java/net/itfans/prototype/web/PrototypeWebApplication.java
@@ -0,0 +1,13 @@
+package net.itfans.prototype.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/itfans/prototype/web/PrototypeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.itfans.prototype.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
